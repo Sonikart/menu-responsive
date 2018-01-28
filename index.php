7fa641efd7630ab4a5486e6cf7cb7f70a10b7d16
Sonik <35891816+Sonikart@users.noreply.github.com>
2018-01-28 18:03:48 +0100

    master

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/stylesheets/style.css">
+    <link rel="stylesheet" href="assets/stylesheets/button.css">
+    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <title>Menu Responsive</title>
+</head>
+<body>
+    <div class="site-container">
+        <div class="site-pusher">
+            <header class="header">
+                <a href="#" class="header__icon" id="header__icon"></a>
+                <a href="#" class="header__logo">Administration | Project</a>
+                <nav class="menu">
+                    <a href="#">Item 1</a>
+                    <a href="#">Item 2</a>
+                    <a href="#">Item 3</a>
+                    <a href="#">Item 4</a>
+                    <a href="#">Item 5</a>
+                </nav>
+            </header>
+            <div class="site-content">
+                <div class="container">
+                    Lorem impsum
+                </div>
+            </div>
+            <div id="site-cache" class="site-cache"></div>
+        </div>
+    </div>
+</body>
+<script type="text/javascript" src="assets/js/jquery-3.3.1.js"></script>
+<script type="text/javascript" src="assets/js/app.js"></script>
+</html>
